90fbdd2dbcd3392e9b92f34e70a77e34a5c14c0e
Dmytro Rabenko <rabenkod@ukr.net>
2024-01-14 09:10:07 +0200

    fix bug width eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended, "plugin:react/jsx-runtime"'],
+  extends: ['eslint:recommended', 'plugin:@next/next/recommended', 'plugin:@typescript-eslint/recommended, "plugin:react/jsx-runtime"'],
   overrides: [
     {
       env: {
