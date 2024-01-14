b217920f1f375f42397ba7d364052e1c79a483d9
Dmytro Rabenko <rabenkod@ukr.net>
2024-01-14 09:20:50 +0200

    no message

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,17 @@
 module.exports = {
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
   env: {
     browser: true,
     es2021: true,
   },
-  extends: ['eslint:recommended', 'plugin:@next/next/recommended', 'plugin:@typescript-eslint/recommended, "plugin:react/jsx-runtime"'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'plugin:@next/next/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
   overrides: [
     {
       env: {
