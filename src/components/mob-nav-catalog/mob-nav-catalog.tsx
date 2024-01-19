4a4f7694fa3f3a1d9e0e4ce887739077537d6fbb
Dmytro Rabenko <rabenkod@ukr.net>
2024-01-19 21:18:03 +0200

    styling some element in mob menu

--- src/components/mob-nav-catalog/mob-nav-catalog.tsx
+++ src/components/mob-nav-catalog/mob-nav-catalog.tsx
@@ -1,51 +1,31 @@
 'use client';
-import Link from 'next/link';
 import useStateStore from '@/store/stateStore';
+import NavCatalogList from '@/components/nav-catalog/nav-catalog-list/nav-catalog-list';
+import Button from '../ui/button/button';
 import ButtonIcon from '@/components/ui/button-icon/button-icon';
 import { Icons } from '@/components/ui/icons/icons';
 import s from './mob-nav-catalog.module.scss';
 
-
 const MobNavCatalog = () => {
   const { isMobCatalogVisible, toggleMobCatalogVisibility } = useStateStore();
-  // useBodyScrollLock(isCatalogVisible);
 
   return (
     <div className={`${s.catalog} ${isMobCatalogVisible ? s.visible : ''}`}>
-      <div className={s.title}>
-        <h2>Каталог товарів</h2>{' '}
+      <div className={s.navigation}>
+        <Button onClick={toggleMobCatalogVisibility}>
+          <Icons.arrowLeft size='small'/>
+          Меню
+        </Button>
+
         <ButtonIcon customClass="light" size="medium" onClick={toggleMobCatalogVisibility}>
           <Icons.close size="medium" />
         </ButtonIcon>
       </div>
 
-      <ul className={s.list}>
-        <li>
-          <Link className={s.instagram} href="/">
-            Чоловічі парфуми
-          </Link>
-        </li>
-        <li>
-          <Link className={s.viber} href="/">
-            Жіночі парфуми
-          </Link>
-        </li>
-        <li>
-          <Link className={s.telegram} href="/">
-            Парфуми для будинку
-          </Link>
-        </li>
-        <li>
-          <Link className={s.phone} href="/">
-            Парфуми в авто
-          </Link>
-        </li>
-        <li>
-          <Link className={s.phone} href="/">
-            Інша парфумерна продукція
-          </Link>
-        </li>
-      </ul>
+      <h2 className={s.title}>Каталог товарів</h2>
+
+      <NavCatalogList />
+      
     </div>
   );
 };
--- src/components/nav-catalog/nav-catalog-list/nav-catalog-list.tsx
+++ src/components/nav-catalog/nav-catalog-list/nav-catalog-list.tsx
@@ -1,5 +1,6 @@
 'use client';
 import Link from 'next/link';
+import { Icons } from '@/components/ui/icons/icons';
 import s from './nav-catalog-list.module.scss';
 
 const NavCatalogList = () => {
@@ -9,26 +10,31 @@ const NavCatalogList = () => {
         <Link className={s.instagram} href="/">
           Чоловічі парфуми
         </Link>
+        <Icons.chevronRight/>
       </li>
       <li>
         <Link className={s.viber} href="/">
           Жіночі парфуми
         </Link>
+        <Icons.chevronRight/>
       </li>
       <li>
         <Link className={s.telegram} href="/">
           Парфуми для будинку
         </Link>
+        <Icons.chevronRight/>
       </li>
       <li>
         <Link className={s.phone} href="/">
           Парфуми в авто
         </Link>
+        <Icons.chevronRight/>
       </li>
       <li>
         <Link className={s.phone} href="/">
           Інша парфумерна продукція
         </Link>
+        <Icons.chevronRight/>
       </li>
     </ul>
   );
